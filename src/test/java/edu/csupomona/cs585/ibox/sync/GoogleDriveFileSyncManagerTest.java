f8d0b7ad66988a1d79edb3a20b9f6e4aaa5f6dcb
jrsilva24 <ruben_prod24@yahoo.com>
2016-10-09 15:12:28 -0700

    Incorrect Behavior

--- src/test/java/edu/csupomona/cs585/ibox/sync/GoogleDriveFileSyncManagerTest.java
+++ src/test/java/edu/csupomona/cs585/ibox/sync/GoogleDriveFileSyncManagerTest.java
@@ -8,8 +8,13 @@ import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import static org.mockito.Mockito.*;
+import edu.csupomona.cs585.ibox.*;
 
+import com.google.api.client.http.AbstractInputStreamContent;
+import com.google.api.client.http.FileContent;
 import com.google.api.services.drive.Drive;
+import com.google.api.services.drive.model.File;
 
 public class GoogleDriveFileSyncManagerTest {
 	
@@ -18,11 +23,42 @@ public class GoogleDriveFileSyncManagerTest {
 	
 	@Before
 	public void setup(){
+		service = GoogleDriveServiceProvider.get().getGoogleDriveClient();
 		MockitoAnnotations.initMocks(this);
 	}
 	
 	@Test
 	public void addFileTest() throws IOException {
+		GoogleDriveFileSyncManager syncManager = new GoogleDriveFileSyncManager(service);
+		java.io.File localFile = new java.io.File ("DummyTest.txt");
+		syncManager.addFile(localFile);
+		
+		File body = new File();
+		body.setTitle(localFile.getName());
+		FileContent mediaContent = new FileContent("*/*", localFile);
+		when(service.files().insert(body, mediaContent).execute()).thenReturn(new File());
+		
+		verify(service).files().insert(body,mediaContent).execute();
+		
+		assertTrue(true);
+	}
+	
+	@Test
+	public void updateFileTest() throws IOException {
+		
+		
+		assertTrue(true);
+	}
+	
+	@Test
+	public void deleteFileTest() throws IOException {
+		
+		
+		assertTrue(true);
+	}
+	
+	@Test
+	public void getFileIdTest() throws IOException {
 		
 		
 		assertTrue(true);
